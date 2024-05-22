1dc23e911d0f32f03683994efb85a42aac8bee8c
AmethystKnight <julianlai2002@gmail.com>
2024-05-22 17:18:25 +1000

    removed unused method

--- logic.py
+++ logic.py
@@ -139,23 +139,6 @@ def tt_entails(self, query):
         models += 1  # Increment models counter
   return True, models
 
-
-
-def tt_check_all(kb, alpha, symbols, model):
-    """Auxiliary routine to implement tt_entails."""
-    if not symbols:
-        if pl_true(kb, model):
-            result = pl_true(alpha, model)
-            assert result in (True, False)
-            return result
-        else:
-            return True
-    else:
-        P, rest = symbols[0], symbols[1:]
-        return (tt_check_all(kb, alpha, rest, extend(model, P, True)) and
-                tt_check_all(kb, alpha, rest, extend(model, P, False)))
-
-
 def prop_symbols(x):
     """Return the set of all propositional symbols in x."""
     if not isinstance(x, Expr):
